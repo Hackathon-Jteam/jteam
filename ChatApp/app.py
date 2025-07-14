fdf4b5668aa8d0e33743278176cdbcff77245ccf
si-ri <0912amethyst@gmail.com>
2025-07-14 22:06:20 +0900

    dockerの起動確認完了

--- ChatApp/app.py
+++ ChatApp/app.py
@@ -1,7 +1,8 @@
 from flask import Flask
 app = Flask(__name__)
-@app.route('/')
+@app.route('/', methods=['GET'])
 def hello_world():
     return '<h1>ハローワールド</h1>'
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", debug=True)
