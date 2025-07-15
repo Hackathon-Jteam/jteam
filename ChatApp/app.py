9a4abaf724a6a8a9eb27287e9a10c544e8da6963
迫田裕介 <sakodayuusuke@sakotayuusukenoMacBook-Air.local>
2025-07-15 22:36:05 +0900

    test

--- ChatApp/app.py
+++ ChatApp/app.py
@@ -1,8 +1,3 @@
-from flask import Flask
-app = Flask(__name__)
-@app.route('/', methods=['GET'])
-def hello_world():
-    return '<h1>ハローワールド</h1>'
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True)
+print('hello')
+elif
+if
