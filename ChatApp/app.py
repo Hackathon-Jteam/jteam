b1119f9702e8f02c73073f10620b0742bbfea1b1
迫田裕介 <sakodayuusuke@sakotayuusukenoMacBook-Air.local>
2025-07-20 14:10:16 +0900

    サインアップ

--- ChatApp/app.py
+++ ChatApp/app.py
@@ -1,7 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
-@app.route('/signup', methods = ['GET'])
-def index ():
+    
+
+
+#サインアップページの表示
+@app.route('/signup', methods = ['POST'])
+def signup ():
     return render_template('signup.html')
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
+
+#サインアップの処理
