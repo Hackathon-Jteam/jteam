3c64030efad5709683591a427c1e6e2802f4894e
迫田裕介 <sakodayuusuke@sakotayuusukenoMacBook-Air.local>
2025-07-21 21:43:21 +0900

    2回目

--- ChatApp/app.py
+++ ChatApp/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
+from db import User, database, session
+from sqlalchemy import*
 app = Flask(__name__)
-    
+db = sqlalchemy(app)
 
 
 
@@ -23,10 +25,30 @@ app = Flask(__name__)
 
 
 #サインアップページの表示
-@app.route('/signup', methods = ['POST'])
+@app.route('/signup', methods = ['GET'])
 def signup ():
     return render_template('signup.html')
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
 
 #サインアップの処理
+@app.route('/signup', methods = ['POST'])
+def signup_process():
+    name = request.form['name']
+    email = request.form['email']
+    nickname = request.form['nickname']
+    password = request.form['password']
+    new_user = User(name = name,email=email,nickname=nickname,password=password)
+    db.session.add(new_user)
+    db.session.commit()
+    return render_template('mypage.html')
+   
+  
+  
+  
+  
+  
+  
+  
+  
+  
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)  
---
+++ ChatApp/db.py
@@ -0,0 +1,49 @@
+import os
+from sqlalchemy import create_engine, Column,Integer,String, or_,DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from datetime import datetime
+
+#DBファイル制作
+base_dir = os.path.dirname(__file__)
+#引数に渡したパスからファイルを除いたフォルダ名を返す(__file__)はdb。py
+database = 'sqlite:///' + os.path.join(base_dir, 'data.sqlite')
+#base_dirとdata.sqliteを繋げて絶対パスを作る　SQLiteの接続先URLは'sqlite:///'＋絶対パス　これをdatabaseに代入
+
+#データベースエンジンの作成
+db_engine = create_engine(database ,echo=True)
+#create_engineはエンジンを作る関数　第一引数は９行目で作ったdatabaseの情報が入っている
+Base = declarative_base()
+#テーブルのクラス作成に必要な関数　SQLAlchemyにクラスとして認識されテーブルが作られない
+
+#テーブル.カラム作成
+class User (Base):
+    __tablename__ = 'users'#テーブル作成
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)#idカラム
+    name = Column(String(255), nullable = False) #SQLAlchemyではVARCHARではなくString
+    email = Column(String(255), nullable = False,unique=True)
+    nickname = Column(String(255), nullable = False , unique=True)
+    password = Column(String(255), nullable = False)
+    created_at = Column(DateTime, nullable = False)
+    updated_at = Column(DateTime, nullable = False)
+
+#コンストラクタ　インスタンスの初期設定をする関数　例　キャラの名前をつける処理
+    def __init__(self, name, email, nickname, password, created_at = None, updated_at = None):
+        self.name = name
+        self.email = email
+        self.nickname = nickname
+        self.password = password
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+#表示用関数   
+    def __str__(self):
+        return f'user(名前:{self.name},メールアドレス:{self.email},ニックネーム:{self.nickname},パスワード:{self.password},登録日時:{self.created_at},更新日時:{self.updated_at})'
+
+#セッションの生成
+session_maker = sessionmaker(bind=db_engine)
+session = session_maker()    
+#テーブル操作    
+#Base.metadata.create_all(db_engine)
+#user01 = User ('yuu', 'hjohs@gmail','masa', 'utyu')
+#session.add_all([user01])
+#session.commit()
