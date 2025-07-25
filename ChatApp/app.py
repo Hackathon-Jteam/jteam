d133db546f8b683776ef9681c463a9eeabe9e5ae
okuzono <mako.to.momo.3762293@gmail.com>
2025-07-25 21:28:06 +0900

    動作確認

--- ChatApp/app.py
+++ ChatApp/app.py
@@ -8,15 +8,12 @@ app = Flask(__name__)
 
 
 
+
+
 #トップページの表示
 @app.route('/', methods = ['GET'])
 def top ():
     return render_template('top.html')
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
-
-
-
 
 
 
